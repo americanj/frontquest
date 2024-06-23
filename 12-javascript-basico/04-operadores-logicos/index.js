898b981f34e69bfad5f21c5b0e1f894666baab8d
Igor Oliveira <lrmcpe22@gmail.com>
2024-06-23 20:30:55 -0300

    operador logicos

---
+++ 12-javascript-basico/04-operadores-logicos/index.js
@@ -0,0 +1,20 @@
+
+
+const a = 3
+const b = '3'
+
+
+console.log(a != b)
+
+
+// nome dele é não identico
+console.log(a !== b)
+
+
+
+console.log(a === b)
+
+
+//Os 3 igual compara o tipo e também o valor do dado.
+
+//nome dele é identico
