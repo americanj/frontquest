154920956223700bdc57b16555c1d793068d58f7
Igor Oliveira <lrmcpe22@gmail.com>
2024-06-22 21:44:07 -0300

    criando variaveis

---
+++ 12-javascript-basico/02-criando-variaveis/index.js
@@ -0,0 +1,21 @@
+{
+    var nome = "igor";
+    const email = 'box@gmail.com';
+    let telefone = '40028922'
+}
+
+console.log(nome);
+
+
+
+let idade = prompt('digite sua idade');
+console.log(idade);
+
+let boolean = confirm('Deseja continuar?');
+console.log(boolean);
+
+
+
+
+
+
