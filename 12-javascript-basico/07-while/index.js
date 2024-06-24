c5718aae260e9f0f6067bceebdede8be807ebfe1
Igor Oliveira <lrmcpe22@gmail.com>
2024-06-23 22:30:02 -0300

    ternario if

---
+++ 12-javascript-basico/07-while/index.js
@@ -0,0 +1,19 @@
+
+let a = 0 
+
+console.log(a);
+
+while (a < 10) {
+    a++;
+}
+
+console.log(a);
+
+
+
+var d_semana = 'sexta';
+
+
+// condição ? console.log("true") : console.log("false")
+d_semana == 'sexta' ? console.log("É SEXTA!!!") : console.log('É um outro dia');
+
