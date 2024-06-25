bc337f0b44897875bdfa9780fbaa56693314fa71
Igor Oliveira <lrmcpe22@gmail.com>
2024-06-24 23:38:49 -0300

    funcao e valor padrao

---
+++ 12-javascript-basico/09-funcoes/index.js
@@ -0,0 +1,24 @@
+
+
+
+function incentivarAluno(nome = 'Aluno') {
+    console.log('Congrats! '+nome+' você chegou aqui');
+}
+
+incentivarAluno();
+
+
+function soma(numero1, numero2) {
+    console.log(numero1 + numero2);
+}
+soma(10, 10);
+
+
+function multiplicar(n1, n2) {
+    return n1 * n2;
+}
+
+let valorMultiplicado = multiplicar(10, 10);
+
+console.log(valorMultiplicado);
+
