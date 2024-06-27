356f2b9970d61a12f40ccb645567267a2560a0bd
Igor Oliveira <lrmcpe22@gmail.com>
2024-06-26 23:08:21 -0300

    viajante exercicio 3

---
+++ 12-javascript-basico/10-exercicios/03-exercicio/index.js
@@ -0,0 +1,5 @@
+
+let escolhaViajante = window.confirm("Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?")
+
+escolhaViajante ? alert("Ótimo! Nós temos as melhores camas de toda a região!") : alert("Que pena! Você parecia ser uma pessoa mais legal")
+
