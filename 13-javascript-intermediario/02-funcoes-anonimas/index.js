ef180c1ae95dc40642d070eae9b94f761d1a6994
Igor Oliveira <lrmcpe22@gmail.com>
2024-06-27 22:41:00 -0300

    funcao anonima

---
+++ 13-javascript-intermediario/02-funcoes-anonimas/index.js
@@ -0,0 +1,15 @@
+
+/*
+function somar(n1, n2) {
+    return n1 + n2;
+}
+
+let resultadoSoma = somar(10, 10);
+*/
+
+
+let resultadoSoma = function(n1, n2) {
+    return n1 + n2;
+}
+
+console.log(resultadoSoma(10,10));
