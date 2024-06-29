61191b0b2a3db6be393287c992158f9a084b7247
Igor Oliveira <lrmcpe22@gmail.com>
2024-06-28 23:47:17 -0300

    funcao arrow

---
+++ 13-javascript-intermediario/03-arrow-function/index.js
@@ -0,0 +1,22 @@
+
+
+let resultadoSoma = (n1, n2) => {
+    return n1 + n2;
+}
+
+console.log(resultadoSoma(10,10));
+
+
+
+const saudar = (nomeQuester) => {
+    console.log(`Parabens ${nomeQuester}`);
+}
+
+saudar("carlos")
+
+
+
+
+let resultadoDivisao = (n1, n2) => n1 / n2;
+
+console.log(resultadoDivisao(10,10));
