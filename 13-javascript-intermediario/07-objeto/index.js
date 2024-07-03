fb58daafa9122d9834f382325f9b8073973d287a
Igor Oliveira <lrmcpe22@gmail.com>
2024-07-03 15:33:13 -0300

    objetos

---
+++ 13-javascript-intermediario/07-objeto/index.js
@@ -0,0 +1,14 @@
+
+
+let tenis = {
+    color: 'white',
+    brand: 'Nike',
+    price: '299.00',
+    estoqueAtivo: true
+}
+
+tenis.tamanho = '44'
+
+console.log(tenis);
+console.log(tenis.brand);
+console.log(tenis['color']);
