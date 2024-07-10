1cb1a521cf80893815be7f90d819af350a46f99f
Igor Oliveira <lrmcpe22@gmail.com>
2024-07-09 21:24:02 -0300

    then

---
+++ 14-javascript-avancado/11-then/index.js
@@ -0,0 +1,39 @@
+/*
+    4 Estados das promises 
+    
+    Pendentes 
+    Realizado resolve
+    Recusada reject
+    Estabelecida 
+**/
+
+// Esses argumentos são funções.
+/*new Promise((resolve, reject) => {
+
+})*/
+
+
+
+let ferverAgua = function(chaleiraNoFogo, fogaoLigado) {
+    return new Promise((resolve, reject) => {
+        if (chaleiraNoFogo && fogaoLigado) {
+            console.log('começando o processo de ferver água.');
+            resolve();
+        } else {
+            console.log('é necessário ligar o fogão e colocar a chaleira no fogão para ferver a água.');
+            reject();
+        }
+    })
+}
+
+
+let chaleiraNoFogo = true;
+let fogaoLigado = true;
+
+let passarCafe = () => console.log('coando café');
+
+
+ferverAgua(chaleiraNoFogo, fogaoLigado)
+.then(passarCafe);
+
+console.log("fazendo café");
