06670d5dda17487efb23cf18d0b8af612a374b93
Igor Oliveira <lrmcpe22@gmail.com>
2024-07-11 16:45:31 -0300

    antes de iniciar com jest

---
+++ 15-testes-automalizados/01-calcular-pedidos/calcular-pedido.js
@@ -0,0 +1,24 @@
+const meuPedido = {
+    items: [
+        { nome: 'Poção de vida', valor: 100 },
+        { nome: 'Espada de prata', valor: 200 },
+        { nome: 'Entrega', valor: 40, entrega: true }
+    ]
+}
+
+const calcularValorPedido = pedido => {
+    const valorDosProdutos = pedido.items.filter(item => !item.entrega)
+    .reduce((totalPedido, pedidoAtual) => totalPedido + pedidoAtual.valor, 0)
+
+                                        //se for um item de entrega pega
+    const entrega = pedido.items.filter(item => item.entrega);
+
+    if (valorDosProdutos > 500) {
+        return valorDosProdutos;
+    } else {
+        return valorDosProdutos + entrega[0].valor;
+    }
+}
+
+
+console.log(calcularValorPedido(meuPedido));
