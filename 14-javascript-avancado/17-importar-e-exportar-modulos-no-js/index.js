080acf1df2734cf6bc90f7786dc067c95e98eeb7
Igor Oliveira <lrmcpe22@gmail.com>
2024-07-11 14:21:20 -0300

    importando e exportando

---
+++ 14-javascript-avancado/17-importar-e-exportar-modulos-no-js/index.js
@@ -0,0 +1,5 @@
+
+
+import { multiplicar, somar } from './operacoes-matematicas.js';
+
+console.log(multiplicar(1, 3));
---
+++ 14-javascript-avancado/17-importar-e-exportar-modulos-no-js/operacoes-matematicas.js
@@ -0,0 +1,12 @@
+
+
+function somar(n1, n2) {
+    return n1+n2;
+}
+
+function multiplicar(n1, n2) {
+    return n1*n2;
+}
+
+export {somar, multiplicar}
+
