a192597cfcd1ba4a40438132cb1a75ed6ca1c40f
americanj <lrmcpe22@gmail.com>
2024-07-17 16:46:25 -0300

    usando props em component de class e de função

--- 17-react-js-base/04-primeiro-projeto-com-react-app-win/src/components/card/card.js
+++ 17-react-js-base/04-primeiro-projeto-com-react-app-win/src/components/card/card.js
@@ -2,15 +2,26 @@ import React from 'react'
 
 import './card.css'
 
-class Card extends React.Component {
+// Usando props em função
+const Card = ({title}) => {
+    return (
+        <div className="card">
+            <h3>{title}</h3>
+            <p>esse é um parágrafo</p>
+        </div>
+    );
+}
+
+/*class Card extends React.Component {
     render() {
+        console.log(this.props);
         return (
             <div className="card">
-                <h3>Título</h3>
+                <h3>{this.props.title}</h3>
                 <p>esse é um parágrafo</p>
             </div>
         );
     }
-}
+}*/
 
 export default Card;
