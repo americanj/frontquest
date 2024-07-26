7fb585d9b8624faed66fe92e03450437fc15b4e2
oliveira404 <lrmcpe22@gmail.com>
2024-07-26 15:23:05 -0300

    utilizando multiplos componentes aninhando eles

--- 17-react-js-base/04-primeiro-projeto-com-react-app-win/src/App.js
+++ 17-react-js-base/04-primeiro-projeto-com-react-app-win/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Button from './components/button/button'
+import Cards from './components/cards/cards';
 
 function App() {
   return (
     <>
-      <Button label='Baixar CV'/>
-      <Button />
+      {/*<Button label='Baixar CV'/>
+      <Button />*/}
+
+        <Cards />
     </>
   )
 }
